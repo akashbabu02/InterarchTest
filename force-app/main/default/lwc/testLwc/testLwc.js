0bcca0ae48bc017a218134cae328e6eaeda56a88
Akash.b <akashbharathi18@gmail.com>
2025-09-16 11:16:30 +0530

    changed

--- force-app/main/default/lwc/testLwc/testLwc.js
+++ force-app/main/default/lwc/testLwc/testLwc.js
@@ -4,6 +4,6 @@ export default class HelloWorld extends LightningElement {
     message;
 
     handleClick() {
-        this.message = 'Button clicked! pipeline has beeen created added delta 🎉';
+        this.message = 'Button clicked! pipeline delta has been added 🎉';
     }
 }
